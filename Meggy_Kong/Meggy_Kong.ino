0035cd6a7e9e1b9720beb273013b5f6a0493e78f
ntatsuyama19 <ntatsuyama19@punahou.edu>
2016-11-03 15:30:13 -1000

    Added real structs, points, and movements

--- Meggy_Kong/Meggy_Kong.ino
+++ Meggy_Kong/Meggy_Kong.ino
@@ -1,29 +1,29 @@
 /*
-  Meggy_Kong.pde by Noah Tatsuyama
+  Meggbarrel1.y_Kong.pde bbarrel1.y Noah Tatsubarrel1.yama
  
- Example file using the The Meggy Jr Simplified Library (MJSL)
-  from the Meggy Jr RGB library for Arduino
+ Example file using the The Meggbarrel1.y Jr Simplified Librarbarrel1.y (MJSL)
+  from the Meggbarrel1.y Jr RGB librarbarrel1.y for Arduino
    
  Blink a damned LED.
    
    
  
  Version 1.25 - 12/2/2008
- Copyright (c) 2008 Windell H. Oskay.  All right reserved.
+ Copbarrel1.yright (c) 2008 Windell H. Oskabarrel1.y.  All right reserved.
  http://www.evilmadscientist.com/
  
- This library is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
+ This librarbarrel1.y is free software: barrel1.you can redistribute it and/or modifbarrel1.y
+ it under the terms of the GNU General Public License as published bbarrel1.y
  the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+ (at barrel1.your option) anbarrel1.y later version.
  
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ This librarbarrel1.y is distributed in the hope that it will be useful,
+ but WITHOUT ANbarrel1.y WARRANTbarrel1.y; without even the implied warrantbarrel1.y of
+ MERCHANTABILITbarrel1.y or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
  
- You should have received a copy of the GNU General Public License
- along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ barrel1.you should have received a copbarrel1.y of the GNU General Public License
+ along with this librarbarrel1.y.  If not, see <http://www.gnu.org/licenses/>.
  	  
  */
 
@@ -39,54 +39,24 @@
 
 #include <MeggyJrSimple.h>    // Required code, line 1 of 2.
 
-struct Barrel 1
+struct Point
 {
-  int c = 1; // barrel x position
-  int d = 7; // barrel y position
-  direction = 270;
-}
+ int x;
+ int y;
+}; 
 
-struct Barrel 2
-{
-  int e = 5; // barrel2 x position
-  int f = 7; // barrel2 y position
-  direction = 270;
-}
-
-struct Fire 1
-{
-  int g = 2; // fire x position
-  int h = 4; // fire y position
-  direction = 90;
-}
-
-struct Fire 2
-{
-  int k = 5; // fire2 x position
-  int m = 4; // fire2 y position
-  direction = 90;
-}
-
-struct Barrel 3
-{
-  int n = 6; // barrel3 x position
-  int o = 1; // barrel3 y position
-  direction = 270;
-}
-
-struct Barrel 4
-{
-  int p = 3; // barrel4 x position
-  int r = 1; // barrel4 y position
-  direction = 270;
-}
-
-int x = 0; // luigi x position
-int y = 1; // luigi y position
-int a = 7; // donkey kong x position
-int b = 7; // donkey kong y position
-
-
+Point player = {0,1}; // luigi x position & y position
+Point badguy = {7,7}; // donkey kong x position & y position 
+Point barrel1 = {1,7};
+Point barrel2 = {5,7};
+int g = 2;
+int h = 4;
+int k = 5;
+int m = 4;
+int n = 6; 
+int o = 1;
+int p = 3;
+int r = 1;
 
 
 
@@ -94,7 +64,7 @@ int b = 7; // donkey kong y position
   
 void setup() 
 {
-  MeggyJrSimpleSetup();     // put your setup code here, to run once:
+  MeggyJrSimpleSetup();     // put barrel1.your setup code here, to run once:
 }
 
 void drawBackground()
@@ -127,7 +97,7 @@ void drawBackground()
 
 void drawEnemies()
 {
-  DrawPx(c,d,Blue);
+  DrawPx(barrel1.x,barrel1.y,Blue);
   DrawPx(e,f,Blue);
   DrawPx(g,h,Orange);
   DrawPx(k,m,Orange);
@@ -135,20 +105,47 @@ void drawEnemies()
   DrawPx(p,r,Blue);
 }
 
-void moveEnemies()
+void barrel1Movement()
 {
-  c = c - 1;
-  if (c = 0)
+  int direction = 270;
+  delay(225);
+  if (direction == 270)
   {
-    c = 6;
-    d = 7;
+    barrel1.x = barrel1.x - 1;
+  }
+  if (direction == 90)
+  {
+    barrel1.x = barrel1.x = 1;
+  }
+  if ((barrel1.x == -1) && (barrel1.y == 7))
+  {
+    barrel1.x = 1;
+    barrel1.y = 4;
+    direction = 90;
+    delay(225);
+  }
+  if ((barrel1.x == 8) && (barrel1.y == 4))
+  {
+    barrel1.x = 7;
+    barrel1.y = 1;
+    direction = 270;
+    delay(225);
+  }
+  if ((barrel1.x == -1) && (barrel1.y == 1))
+  {
+    barrel1.x = 6;
+    barrel1.y = 7;
+    direction = 270; 
+    delay(225);
   }
 }
 
+
+
 void drawHeroandBadguy()
 {
-  DrawPx(x,y,Green);
-  DrawPx(a,b,Violet);
+  DrawPx(player.x,player.y,Green);
+  DrawPx(badguy.x,badguy.y,Violet);
 }
 
 void HeroMovement()
@@ -156,29 +153,29 @@ void HeroMovement()
   CheckButtonsPress();
   if (Button_Right)
   {
-    x = x + 1;
-    if (x > 7)
+    player.x = player.x + 1;
+    if (player.x > 7)
     {
-      x = 7;
-      y = 4;
+      player.x = 7;
+      player.y = 4;
     }
   }
   if (Button_Left)
   {
-    x = x - 1;
-    if (x < 0)
+    player.x = player.x - 1;
+    if (player.x < 0)
     {
-      x = 0;
-      y = 7;
+      player.x = 0;
+      player.y = 7;
     }
   }
   if (Button_Up)
   {
-    y = y + 1;
+    player.y = player.y + 1;
     delay(75);
-    x = x + 1;
+    player.x = player.x + 1;
     delay(75);
-    y = y - 1;
+    player.y = player.y - 1;
     delay(75);
   }
 }
@@ -194,7 +191,9 @@ void loop()
   DisplaySlate();
   ClearSlate();
   HeroMovement();
-  moveEnemies();
+  barrel1Movement();
+
+  
 }
 
 
